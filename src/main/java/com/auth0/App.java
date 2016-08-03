d1e92d5917ecfb881fd9756cc6b576280d1abe8c
Hernan Zalazar <hernan.zalazar@gmail.com>
2016-08-03 15:00:02 -0300

    Initial project structure

---
+++ src/main/java/com/auth0/App.java
@@ -0,0 +1,13 @@
+package com.auth0;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
