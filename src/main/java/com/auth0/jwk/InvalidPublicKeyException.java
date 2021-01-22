2eb9caf0d3925a40512226ca81b15fe6cef76428
jestum <jesse.estum@optum.com>
2021-01-22 15:40:36 -0600

    Add support for Elliptic Curve algorithm public keys

--- src/main/java/com/auth0/jwk/InvalidPublicKeyException.java
+++ src/main/java/com/auth0/jwk/InvalidPublicKeyException.java
@@ -3,6 +3,10 @@ package com.auth0.jwk;
 @SuppressWarnings("WeakerAccess")
 public class InvalidPublicKeyException extends JwkException {
 
+    public InvalidPublicKeyException(String msg) {
+        super(msg);
+    }
+
     public InvalidPublicKeyException(String msg, Throwable cause) {
         super(msg, cause);
     }
