d5e5aa9d67a8f24030bba3f5801dfdc2806b3692
Jim Anderson <jimranderson@gmail.com>
2022-09-06 20:26:29 -0500

    Clarify that getting a JWK is a synchronous operation

--- src/main/java/com/auth0/jwk/JwkProvider.java
+++ src/main/java/com/auth0/jwk/JwkProvider.java
@@ -6,9 +6,10 @@ package com.auth0.jwk;
 @SuppressWarnings("WeakerAccess")
 public interface JwkProvider {
     /**
-     * Returns a jwk using the kid value
-     * @param keyId value of kid found in JWT
-     * @return a jwk
+     * Attempts to get a JWK using the Key ID value. Note that implementations are synchronous (blocking).
+     *
+     * @param keyId value of the kid found in a JWT
+     * @return a JWK
      * @throws SigningKeyNotFoundException if no jwk can be found using the given kid
      */
     Jwk get(String keyId) throws JwkException;
